18bf4836f8e78581123f7f2143410ed320c5132c
Babak Gholamzadeh <babak.gholamzadeh92@gmail.com>
2021-03-08 01:47:32 +0330

    events: implement methods for adding event listener

--- events.js
+++ events.js
@@ -2,6 +2,19 @@ class EventEmitter {
   constructor() {
     this._events = {};
   }
+
+  addListener(eventName, listener) {
+    const events = this._events;
+    if (!events[eventName])
+      events[eventName] = [];
+    events[eventName].push(listener);
+    return this;
+  }
+
+  on(eventName, listener) {
+    return this.addListener(eventName, listener);
+  }
+
 }
 
 module.exports = EventEmitter;
