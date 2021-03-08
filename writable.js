fb8ee7cc7d6729d0a7a9a57039f1e36a9a66cc4f
Babak Gholamzadeh <babak.gholamzadeh92@gmail.com>
2021-03-08 09:37:47 +0330

    writable: basic implementation of write method to act as a stright data transfer

--- writable.js
+++ writable.js
@@ -1,5 +1,11 @@
 class Writable {
-  constructor() {}
+  constructor() { }
+  
+  write(chunk) {
+    if (typeof this._write !== 'function')
+      throw new Error('_write() must to be implemented!');
+    this._write(chunk);
+  }
 }
 
 module.exports = Writable;
