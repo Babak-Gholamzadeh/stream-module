aba4af402ec4d93414dd5e18bd504d66d8d9c49c
Babak Gholamzadeh <babak.gholamzadeh92@gmail.com>
2021-03-08 21:14:46 +0330

    writable: define highWaterMark and emit drain event to prevent the buffer size exceeding

--- writable.js
+++ writable.js
@@ -1,28 +1,43 @@
+const EventEmitter = require('./events');
 class WritableState {
-  constructor() {
+  constructor({ highWaterMark = 16 * 1024 }) {
+    this.highWaterMark = highWaterMark;
+    this.length = 0;
     this.buffered = [];
     this.writing = false;
     this.writecb = null;
+    this.writelen = 0;
+    this.needDrain = false;
   }
 }
-class Writable {
-  constructor() {
-    this._writableState = new WritableState();
+class Writable extends EventEmitter {
+  constructor(options = {}) {
+    super();
+    this._writableState = new WritableState(options);
   }
-  
+
   write(chunk, callback) {
     const state = this._writableState;
 
     if (typeof this._write !== 'function')
       throw new Error('_write() must to be implemented!');
     
+    const len = chunk.length;
+    state.length += len;
+    const ret = state.length < state.highWaterMark;
+    if (!ret)
+      state.needDrain = true;
+
     if (state.writing) {
       state.buffered.push({ chunk, callback });
     } else {
       state.writing = true;
       state.writecb = callback;
+      state.writelen = len;
       this._write(chunk, this._onwrite.bind(this));
     }
+
+    return ret;
   }
 
   _onwrite() {
@@ -32,9 +47,16 @@ class Writable {
 
     state.writing = false;
     state.writecb = null;
+    state.length -= state.writelen;
+    state.writelen = 0;
 
     if (state.buffered.length) {
       this._clearBuffer();
+    }
+
+    if (state.needDrain && state.length === 0) {
+      state.needDrain = false;
+      this.emit('drain');
     }
 
     if (typeof cb === 'function') {
