2953fa04dd565f1a2178c8c002b1555357377fb2
Babak Gholamzadeh <babak.gholamzadeh92@gmail.com>
2021-03-08 10:32:19 +0330

    writable: avoid to send data to destination when it's still on writing by buffering the data

--- writable.js
+++ writable.js
@@ -1,10 +1,31 @@
+class WritableState {
+  constructor() {
+    this.buffered = [];
+    this.writing = false;
+  }
+}
 class Writable {
-  constructor() { }
+  constructor() {
+    this._writableState = new WritableState();
+  }
   
   write(chunk) {
+    const state = this._writableState;
+
     if (typeof this._write !== 'function')
       throw new Error('_write() must to be implemented!');
-    this._write(chunk);
+    
+    if (state.writing) {
+      state.buffered.push(chunk);
+    } else {
+      state.writing = true;
+      this._write(chunk, this._onwrite.bind(this));
+    }
+  }
+
+  _onwrite() {
+    const state = this._writableState;
+    state.writing = false;
   }
 }
 
