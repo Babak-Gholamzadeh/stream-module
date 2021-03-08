65f8817ce302c23c317ba495a04f17e759521097
Babak Gholamzadeh <babak.gholamzadeh92@gmail.com>
2021-03-08 14:52:00 +0330

    writable: define a callback as a parameter of write method to be called inside of _onwirte method

--- writable.js
+++ writable.js
@@ -2,6 +2,7 @@ class WritableState {
   constructor() {
     this.buffered = [];
     this.writing = false;
+    this.writecb = null;
   }
 }
 class Writable {
@@ -9,37 +10,48 @@ class Writable {
     this._writableState = new WritableState();
   }
   
-  write(chunk) {
+  write(chunk, callback) {
     const state = this._writableState;
 
     if (typeof this._write !== 'function')
       throw new Error('_write() must to be implemented!');
     
     if (state.writing) {
-      state.buffered.push(chunk);
+      state.buffered.push({ chunk, callback });
     } else {
       state.writing = true;
+      state.writecb = callback;
       this._write(chunk, this._onwrite.bind(this));
     }
   }
 
   _onwrite() {
     const state = this._writableState;
+
+    const cb = state.writecb;
+
     state.writing = false;
+    state.writecb = null;
+
     if (state.buffered.length) {
       this._clearBuffer();
+    }
+
+    if (typeof cb === 'function') {
+      cb();
     }
   }
 
   _clearBuffer() {
     const state = this._writableState;
-    const chunk = state.buffered.shift();
-    this._doWrite(chunk);
+    const { chunk, callback } = state.buffered.shift();
+    this._doWrite(chunk, callback);
   }
 
-  _doWrite(chunk) {
+  _doWrite(chunk, callback) {
     const state = this._writableState;
     state.writing = true;
+    state.writecb = callback;
     this._write(chunk, this._onwrite.bind(this));
   }
 
