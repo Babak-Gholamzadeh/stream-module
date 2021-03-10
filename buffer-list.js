bb69be80ed771ebcc9176391efa164b2147ad855
Babak Gholamzadeh <babak.gholamzadeh92@gmail.com>
2021-03-10 15:50:30 +0330

    buffer-list: implement a Node class to instantiate new nodes for the queue

--- buffer-list.js
+++ buffer-list.js
@@ -1,5 +1,15 @@
+class Node {
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+}
 class BufferList {
-  constructor() {}
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
 }
 
 module.exports = BufferList;
