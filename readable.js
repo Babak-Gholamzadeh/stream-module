71d26eb30b86981c0041449794ef904756591a6c
Babak Gholamzadeh <babak.gholamzadeh92@gmail.com>
2021-03-11 13:23:04 +0330

    readable: define two flowing modes
    - use BufferList module to buffering the data in memory
    - add resume method to start the flowing
    - add read method to consume buffered data after start flowing
    - update push method to match with the two flowing mode

--- readable.js
+++ readable.js
@@ -1,11 +1,48 @@
 const EventEmitter = require('./events');
+const BufferList = require('./buffer-list');
+class ReadableState {
+  constructor() {
+    this.buffer = new BufferList();
+    this.length = 0;
+    this.flowing = null;
+  }
+}
+
 class Readable extends EventEmitter {
   constructor() {
     super();
+    this._readableState = new ReadableState();
   }
 
   push(chunk) {
-    this.emit('data', chunk);
+    const state = this._readableState;
+    if (state.flowing && this.listenerCount('data') > 0 && state.length === 0) {
+      this.emit('data', chunk);
+    } else {
+      state.length += chunk.length;
+      state.buffer.push(chunk);
+    }
+  }
+
+  resume() {
+    const state = this._readableState;
+    state.flowing = true;
+    while (this.read() !== null);
+  }
+
+  read() {
+    const state = this._readableState;
+
+    let ret = null;
+    if (state.length > 0)
+      ret = state.buffer.shift();
+
+    if (ret !== null) {
+      state.length -= ret.length;
+      this.emit('data', ret);
+    }
+
+    return ret;
   }
 }
 
