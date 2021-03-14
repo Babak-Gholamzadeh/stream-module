20e8f4adcdbecdbbd67761ec562ec0ecabf7bbd3
Babak Gholamzadeh <babak.gholamzadeh92@gmail.com>
2021-03-14 19:37:27 +0330

    transform: implement _write method to start the transferring

--- transform.js
+++ transform.js
@@ -3,6 +3,23 @@ const Duplex = require('./duplex');
 class Transform extends Duplex {
   constructor(options) {
     super(options);
+    this._callback = null;
+  }
+
+  _write(chunk, encoding, callback) {
+    this._transform(chunk, encoding, (err, val) => {
+      if (err) {
+        callback(err);
+        return;
+      }
+      if (val !== null)
+        this.push(val);
+      this._callback = callback;
+    });
+  }
+
+  _transform() {
+    throw new Error('_transform method must be implemented!');
   }
 }
 
